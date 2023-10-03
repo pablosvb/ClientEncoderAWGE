4045f7c29271d4580be07b3bddb963fff50a7ab0
pablosvb1 <pablosvb1@gmail.com>
2023-10-03 13:45:53 +0200

    s

--- src/infrastucture/menu_handler.py
+++ src/infrastucture/menu_handler.py
@@ -237,7 +237,7 @@ class MenuHandler:
                     else:
                         draw.text((x_position, 50), " " , font=font, fill="white")
 
-    def format_with_spaces(n):
+    def format_with_spaces(self,n):
         s = "{:011}".format(n)
         return ' '.join([s[i:i+3] for i in range(0, len(s), 3)])
 
