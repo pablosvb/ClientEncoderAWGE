d4b643edac1fdecd25d33f4a10d3ab38e1017305
pablosvb1 <pablosvb1@gmail.com>
2023-10-03 02:54:38 +0200

    s

--- src/infrastucture/menu_handler.py
+++ src/infrastucture/menu_handler.py
@@ -159,7 +159,7 @@ class MenuHandler:
     def display_option_Status(self):
         if self.Menu_option_Status == 0:
             with canvas(self.device) as draw: 
-                draw.rectangle([(10, 0), (130, 25)], outline="white")
+                draw.rectangle([(0, 0), (120, 25)], outline="white")
                 draw.text((30,0), "Frecuencia:  ", font=font, fill="white")
                 draw.text((10,10),"{:011}".format(self.frecuencia)+"Hz",font=font, fill="white")
                 draw.text((0,30), "RF_Enable: "+str(bool(self.rf_enable)), font=font, fill="white")
