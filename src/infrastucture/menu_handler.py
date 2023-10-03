aa812e45c6c41879836af85b451ae3f6bd53adc4
pablosvb1 <pablosvb1@gmail.com>
2023-10-03 13:27:58 +0200

    s

--- src/infrastucture/menu_handler.py
+++ src/infrastucture/menu_handler.py
@@ -188,13 +188,13 @@ class MenuHandler:
         if self.Menu_option_Status == 0:
             with canvas(self.device) as draw: 
                 draw.rectangle([(0, 0), (127, 25)], outline="white")
-                draw.text((30,0), "Frecuencia:  ", font=font_status, fill="white")
-                draw.text((17,10),"{:011}".format(self.frecuencia)+"Hz",font=font_status, fill="white")
+                draw.text((20,0), "Frec. Sintetizador:  ", font=font_status, fill="white")
+                draw.text((17,10),"{:011}".format(self.frecuencia)+" Hz",font=font_status, fill="white")
                 draw.text((0,26), "RF_Enable: "+str(bool(self.rf_enable)), font=font_status, fill="white")
-                draw.line([(0, 38), (128, 38)], fill="white")
-                draw.text((0,38),"Power: "+str(self.potencia)+"dB",font=font_status, fill="white")
-                draw.line([(0, 50), (128, 50)], fill="white")
-                draw.text((0,50), "ALC_Mode:"+self.ALC_mode, font=font_status, fill="white")
+                #draw.line([(0, 38), (128, 38)], fill="white")
+                draw.text((0,38),"Power: "+str(self.potencia)+"dBm",font=font_status, fill="white")
+                #draw.line([(0, 50), (128, 50)], fill="white")
+                draw.text((0,50), "Ref:"+self.ALC_mode, font=font_status, fill="white")
         elif self.Menu_option_Status == 1:
             with canvas(self.device) as draw: 
                 draw.text((0, 0), "Att_RCP:  "+str(self.Att_RCP)+"dB", font=font_status, fill="white")
