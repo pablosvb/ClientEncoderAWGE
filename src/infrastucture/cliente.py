122c41a947a90dbb30e16381e60d83d3579247e4
pablosvb1 <pablosvb1@gmail.com>
2023-10-03 04:20:26 +0200

    s

--- src/infrastucture/cliente.py
+++ src/infrastucture/cliente.py
@@ -1,19 +1,24 @@
 from src.infrastucture.Conection import Conection
 import threading
 from .menu_handler import MenuHandler  # Importar la clase desde el archivo
+from queue import Queue
+
+message_queue = Queue()
+
+menu_handler_instance = MenuHandler(message_queue)
+connection_instance = Conection(message_queue)
+
 
 class cliente():
 
     def __init__(self):
-        def start_menu():
-            menu = MenuHandler()
-            menu.run()
         try:
-            menu_thread = threading.Thread(target=start_menu)
+            menu_thread = threading.Thread(target=menu_handler_instance.run)
+            conection_thread = threading.Thread(target=connection_instance.conexion(("127.0.0.1",7000)))
             menu_thread.start()
+            conection_thread.start()
         except KeyboardInterrupt:
             pass
         finally:
-            Conection.conexion("127.0.0.1",7000)
 
 
