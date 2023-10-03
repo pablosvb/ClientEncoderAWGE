1db1ba0c122d6101afa5382b9dd9d3de680ab90a
pablosvb1 <pablosvb1@gmail.com>
2023-10-03 07:04:53 +0200

    s

--- src/infrastucture/Conection.py
+++ src/infrastucture/Conection.py
@@ -23,52 +23,50 @@ class Conection:
         return PORT
     
     def conexion(self):
-        try:
-            HOST = "127.0.0.1"
-            PORT = 7000
-            error = False;
-            logger.debug("Conectando...")
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(("127.0.0.1", 7000))
-            logger.debug(f'Conectado al servidor: {HOST}:{PORT}')
-            
-            while not self.terminate_event.is_set():
-                # Introducimos los mensajes que se necesiten desde el terminal.
-                mensaje = self.queue_m_c.get()
-                print("imprimiendo mensaje:"+ mensaje)
-                s.sendall(mensaje.encode())
-                data = s.recv(1024)
-                if mensaje == "get_status_cm":
-                    self.queue_c_m.put(data.decode())
-                print(f'Server:{data.decode()}')
-
-                    
-        except socket.gaierror as e:
-            error = True
-            if e.errno == 11001:
-                logger.debug('ERROR 11001: Los Datos de HOST Y PUERTO introducidos no son correctos')
-            else:
-                logger.debug(f'Error gaierror: {e}')
-        except socket.timeout as e:
-            error = True
-            if e.errno == 10060:
-                logger.debug('ERROR 10060: Se ha agotado el tiempo de espera para la conexión. El servidor no ha respondido a tiempo., puede que introduzca mal el HOST u/o el PORT')
-            elif e.errno == 10061:
-                logger.debug("ERROR 10061: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión, puede que introduzca mal el HOST u/o el PORT")
         
-        except Exception as e:
-            error = True
-            logger.debug(f'ERROR: {e}')
-        finally:
+        HOST = "127.0.0.1"
+        PORT = 7000
+        error = False
+        while not self.terminate_event.is_set():  # Principal loop de ejecución
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
+                logger.debug("Conectando...")
+                s.connect((HOST, PORT))
+                logger.debug(f'Conectado al servidor: {HOST}:{PORT}')
+
+                while not self.terminate_event.is_set():
+                    mensaje = self.queue_m_c.get()
+                    print("imprimiendo mensaje:"+ mensaje)
+                    s.sendall(mensaje.encode())
+                    data = s.recv(1024)
+                    if mensaje == "get_status_cm":
+                        self.queue_c_m.put(data.decode())
+                    print(f'Server:{data.decode()}')
+
+            except socket.gaierror as e:
+                error = True
+                if e.errno == 11001:
+                    logger.debug('ERROR 11001: Los Datos de HOST Y PUERTO introducidos no son correctos')
+                else:
+                    logger.debug(f'Error gaierror: {e}')
+
+            except socket.timeout as e:
+                error = True
+                if e.errno == 10060:
+                    logger.debug('ERROR 10060: Se ha agotado el tiempo de espera para la conexión. El servidor no ha respondido a tiempo., puede que introduzca mal el HOST u/o el PORT')
+                elif e.errno == 10061:
+                    logger.debug("ERROR 10061: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión, puede que introduzca mal el HOST u/o el PORT")
+
+            except Exception as e:
+                error = True
+                logger.debug(f'ERROR: {e}')
+
+            finally:
                 s.close()
-                if (error == True):
-                    logger.debug("Esperando para Reconexion ... ")
-                    if mensaje == "salir":
-                        self.terminate_event.set()
-                        pass
-                    time.sleep(2)
-                    self.conexion()
-            except:
-                pass
+
+            if error and not self.terminate_event.is_set():
+                logger.debug("Esperando para Reconexion ... ")
+                if mensaje == "salir":
+                    self.terminate_event.set()
+                time.sleep(2)
     
