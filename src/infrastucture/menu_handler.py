558bbcc3dd6b38ccd70754b6087791608a3af30b
pablosvb1 <pablosvb1@gmail.com>
2023-10-09 12:05:08 +0200

    s

--- src/infrastucture/menu_handler.py
+++ src/infrastucture/menu_handler.py
@@ -268,7 +268,7 @@ class MenuHandler:
     
     def display_option_Ref_Clock(self):
         # Calcula el tamaño de los textos y de la imagen:
-        textwidth_title = font.getlength("Modo Ref_Clock:")
+        textwidth_title = font.getlength("Ref_Clock:"+self.ext_ref_detect)
         textwidth_selection = font.getlength("External   Internal")
         
         # Calcula las coordenadas x,centrar el texto
@@ -276,7 +276,7 @@ class MenuHandler:
         x_selection = (device.width - textwidth_selection) // 2
 
         with canvas(self.device) as draw: 
-            draw.text((x_title, 0), "Modo Ref_Clock:", font=font, fill="white")
+            draw.text((x_title, 0), "Modo Ref_Clock: "+self.ext_ref_detect, font=font, fill="white")
             draw.text((x_selection,40),"External   Internal",font=font, fill="white")
             for i, option in enumerate(self.options_Ref_Clock):
                 x_position = 85 - i * 55
